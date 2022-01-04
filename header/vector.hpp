6b0bcdc7866b60733b2d28b860f83b0a8a3658cf
agautier <agautier@student.42.fr>
2022-01-04 12:43:51 +0100

    fix(vector): rename Vector to vector

---
+++ header/vector.hpp
@@ -0,0 +1,147 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 14:06:38 by agautier          #+#    #+#             */
+/*   Updated: 2022/01/04 12:43:23 by agautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <cstddef>
+#include <memory>
+
+namespace ft {
+
+template< class T, class Allocator = std::allocator< T > >
+class vector {
+		// TODO: public / private
+		// TODO: rename to vector
+	public:
+		class vectorIterator {};
+
+		/* Member types */
+
+		typedef T								  value_type;
+		typedef Allocator						  allocator_type;
+		typedef std::size_t						  size_type;
+		typedef std::ptrdiff_t					  difference_type;
+		typedef value_type&						  reference;
+		typedef value_type const&				  const_reference;
+		typedef typename Allocator::pointer		  pointer;
+		typedef typename Allocator::const_pointer const_pointer;
+		// typedef iterator;
+		// typedef const_iterator;
+		// typedef std::reverse_iterator< iterator >		reverse_iterator;
+		// typedef std::reverse_iterator< const_iterator >
+		// const_reverse_iterator;
+
+		// TODO: iterators
+
+		/* Member functions */
+		// vector();
+		vector(void) {}
+		// explicit vector(Allocator const& alloc);
+		// explicit vector(size_type		 count,
+		// 				const T&		 value = T(),
+		// 				Allocator const& alloc = Allocator());
+		// template< class InputIt >
+		// vector(InputIt			first,
+		// 	   InputIt			last,
+		// 	   Allocator const& alloc = Allocator());
+		// vector(vector const& other);
+		vector(vector const& other) { *this = other; }
+		// ~vector();
+		~vector(void) {
+			// TODO: This destroys all container elements, and
+			// deallocates all the storage capacity allocated by
+			// the vector using its allocator.
+		}
+		// vector& operator=( const vector& other );
+		vector& operator=(vector const& other) {
+			if (this == &other)
+				return (*this);
+			// TODO: add values
+			return (*this);
+		}
+		// void assign( size_type count, const T& value );
+		// template< class InputIt >
+		// void assign( InputIt first, InputIt last );
+		// allocator_type get_allocator() const;
+
+		vector(size_type n, value_type const& val = value_type()) {
+			// TODO: implement
+			(void)n;
+			(void)val;
+		}
+
+		/* Element access */
+
+		// reference at( size_type pos );
+		// const_reference at( size_type pos ) const;
+		// reference operator[]( size_type pos );
+		// const_reference operator[]( size_type pos ) const;
+		// reference front();
+		// const_reference front() const;
+		// reference back();
+		// const_reference back() const;
+		// T* data();
+		// const T* data() const;
+
+		/* Iterators */
+
+		// iterator begin();
+		// const_iterator begin() const;
+		// iterator end();
+		// const_iterator end() const;
+		// reverse_iterator rbegin();
+		// const_reverse_iterator rbegin() const;
+		// reverse_iterator rend();
+		// const_reverse_iterator rend() const;
+
+		/* Capacity */
+
+		// bool empty() const;
+		// size_type size() const;
+		// size_type max_size() const;
+		// void reserve( size_type new_cap );
+		// size_type capacity() const;
+
+		/* Modifiers */
+
+		// void clear();
+		// iterator insert( iterator pos, const T& value );
+		// void insert( iterator pos, size_type count, const T& value );
+		// template< class InputIt >
+		// void insert( iterator pos, InputIt first, InputIt last );
+		// iterator erase( iterator pos );
+		// iterator erase( iterator first, iterator last );
+		// void push_back( const T& value );
+		// void pop_back();
+		// void resize( size_type count, T value = T() );
+		// void swap( vector& other );
+
+		/* Non member function overload */
+
+		// template< class T, class Alloc >
+		// void swap( std::vector<T,Alloc>& lhs, std::vector<T,Alloc>& rhs );
+		// template< class T, class Alloc >
+		// bool operator==(std::vector< T, Alloc > const& lhs, std::vector< T,
+		// Alloc > const& rhs); template< class T, class Alloc > bool
+		// operator==(std::vector< T, Alloc > const& lhs, std::vector< T, Alloc
+		// > const& rhs); template< class T, class Alloc > bool
+		// operator<(std::vector< T, Alloc > const& lhs, std::vector< T, Alloc >
+		// const& rhs); template< class T, class Alloc > bool
+		// operator<=(std::vector< T, Alloc > const& lhs, std::vector< T, Alloc
+		// > const& rhs); template< class T, class Alloc > bool
+		// operator>(std::vector< T, Alloc > const& lhs, std::vector< T, Alloc >
+		// const& rhs); template< class T, class Alloc > bool
+		// operator>=(std::vector< T, Alloc > const& lhs, std::vector< T, Alloc
+		// > const& rhs);
+};
+
+} // namespace ft
--- test/vector.cpp
+++ test/vector.cpp
@@ -6,11 +6,11 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 20:31:25 by agautier          #+#    #+#             */
-/*   Updated: 2021/12/22 17:47:14 by agautier         ###   ########.fr       */
+/*   Updated: 2022/01/04 12:40:34 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Vector.hpp"
+#include "vector.hpp"
 #include "Test.hpp"
 #include <iostream>
 #include <stdint.h>
@@ -19,7 +19,7 @@
 // Member functions
 
 bool test_default_constructor(void) {
-	ft::Vector< int > ftvec;
+	ft::vector< int > ftvec;
 	(void)ftvec;
 	return (true);
 }
@@ -27,21 +27,21 @@ bool test_default_constructor(void) {
 bool test_fill_constructor(void) {
 	std::size_t		  n	  = 10;
 	int				  val = 42;
-	ft::Vector< int > ftvec(n, val);
+	ft::vector< int > ftvec(n, val);
 	(void)ftvec;
 	return (true);
 }
 
 // bool test_range_constructor(void) {
 //	std::vector< int > stdvec(10, 42);
-//	ft::Vector< int > ftvec(stdvec.begin(), stdvec.end());
+//	ft::vector< int > ftvec(stdvec.begin(), stdvec.end());
 //	(void)ftvec;
 //	return (true);
 // }
 
 bool test_copy_constructor(void) {
-	ft::Vector< int > ftvec;
-	ft::Vector< int > ftvec2(ftvec);
+	ft::vector< int > ftvec;
+	ft::vector< int > ftvec2(ftvec);
 	(void)ftvec2;
 	return (true);
 }
@@ -49,21 +49,21 @@ bool test_copy_constructor(void) {
 // Capacity
 
 // bool test_default_constructor_empty(void) {
-//	ft::Vector< int > ftvec;
+//	ft::vector< int > ftvec;
 //	std::vector< int > stdvec;
 //
 //	assert_eq(ftvec.empty(), stdvec.empty());
 // }
 //
 // bool test_default_constructor_size(void) {
-//	ft::Vector< int > ftvec;
+//	ft::vector< int > ftvec;
 //	std::vector< int > stdvec;
 //
 //	assert_eq(ftvec.size(), stdvec.size());
 // }
 
 bool test_vector(void) {
-	Test test("Vector");
+	Test test("vector");
 
 	// Member functions
 	test.registerTest(&test_default_constructor);
@@ -80,10 +80,10 @@ bool test_vector(void) {
 	// Modifiers
 	// Allocator
 
-	// ft::Vector< int > second(4, 100); // four ints with value 100
-	// ft::Vector< int > third(second.begin(),
+	// ft::vector< int > second(4, 100); // four ints with value 100
+	// ft::vector< int > third(second.begin(),
 	//						second.end()); // iterating throuth second
-	// ft::Vector< int > fourth(third); // a copy of third
+	// ft::vector< int > fourth(third); // a copy of third
 
 	return (test.run());
 }
