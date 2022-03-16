5b53b994ab2850365be178eeda79c762521bc4b8
Marvyn <56036468+Marvyn42@users.noreply.github.com>
2022-03-16 22:00:47 +0100

    fix: vector equal operator (#1)
    Ok, merci BG.

--- header/algorithm.hpp
+++ header/algorithm.hpp
@@ -6,8 +6,8 @@ namespace ft
 template< class InputIt1, class InputIt2 >
 bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 )
 {
-	for (InputIt1 it = first1; first1 != last1; ++it)
-		if (*it != *first2)
+	for (InputIt1 it = first1; it != last1; ++it)
+		if (*it != *first2++)
 			return (false);
 	return (true);
 }
@@ -15,8 +15,8 @@ bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 )
 template< class InputIt1, class InputIt2, class BinaryPredicate >
 bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p )
 {
-	for (InputIt1 it = first1; first1 != last1; ++it && ++first2)
-		if (!p(*it, *first2))
+	for (InputIt1 it = first1; first1 != last1; ++it)
+		if (!p(*it, *first2++))
 			return (false);
 	return (true);
 }
